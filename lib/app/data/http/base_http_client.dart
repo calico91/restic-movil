5a344e6f1273affbc2e6db52c9e4f9155e258689
Cristian Andres <barcandres91@gmail.com>
2026-05-15 12:52:59 -0500

    se actualiza apikey

--- lib/app/data/http/base_http_client.dart
+++ lib/app/data/http/base_http_client.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:flutter/rendering.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
